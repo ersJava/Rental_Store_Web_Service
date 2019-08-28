df558dd77a8c342431192d92cdcff62215cb7dfb
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-08-28 15:46:55 -0400

    added additional prepared statements

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceDaoJdbcImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceDaoJdbcImpl.java
@@ -2,6 +2,7 @@ package com.company.U1M6Summative.dao;
 
 import com.company.U1M6Summative.model.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,80 +13,81 @@ import java.util.List;
 
 @Repository
 public class InvoiceDaoJdbcImpl implements InvoiceDao {
-    @Override
-    public Invoice addInvoice(Invoice invoice) {
-        return null;
+
+    private JdbcTemplate jdbcTemplate;
+
+    //prepared statements
+    private static final String INSERT_INVOICE_SQL =
+            "insert into invoice (customer_id, order_date, pickup_date, return_date, late_fee) values (?, ?, ?, ?, ?)";
+
+    private static final String DELETE_INVOICE_SQL =
+            "delete from invoice where invoice_id = ?";
+
+    private static final String SELECT_INVOICES_BY_CUSTOMER_SQL =
+            "select * from invoice where customer_id = ?";
+
+    private static final String SELECT_INVOICE_SQL =
+            "select * from invoice where invoice_id =?";
+
+    private static final String SELECT_ALL_INVOICES_SQL =
+            "select * from invoice";
+
+    private static final String UPDATE_INVOICE_SQL =
+            "update invoice set customer_id = ?, order_date = ?, pickup_date = ?, return_date = ?, late_fee = ? where invoice_id = ?";
+
+    @Autowired
+    public InvoiceDaoJdbcImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private Invoice mapRowToInvoice(ResultSet rs, int rowNum) throws SQLException {
+        Invoice invoice = new Invoice();
+        invoice.setInvoiceId(rs.getInt("invoice_id"));
+        invoice.setCustomerId(rs.getInt("customer_id"));
+        invoice.setOrderDate(rs.getObject("order_date", LocalDate.class));
+        invoice.setPickupDate(rs.getObject("pickup_date", LocalDate.class));
+        invoice.setReturnDate(rs.getObject("return_date", LocalDate.class));
+        invoice.setLateFee(rs.getBigDecimal("late_fee"));
+
+        return invoice;
     }
 
     @Override
-    public Invoice getInvoiceById(int invoice_id) {
-        return null;
+    public Invoice addInvoice(Invoice invoice) {
+        jdbcTemplate.update(INSERT_INVOICE_SQL, invoice.getCustomerId(), invoice.getOrderDate(), invoice.getPickupDate(), invoice.getReturnDate()
+                , invoice.getLateFee());
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+        invoice.setInvoiceId(id);
+        return invoice;
+    }
+
+    @Override
+    public Invoice getInvoiceById(int invoiceId) {
+        try {
+            return jdbcTemplate.queryForObject(SELECT_INVOICE_SQL, this::mapRowToInvoice, invoiceId);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public List<Invoice> getAllInvoices() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_INVOICES_SQL, this::mapRowToInvoice);
     }
 
     @Override
     public void updateInvoice(Invoice invoice) {
-
+        jdbcTemplate.update(UPDATE_INVOICE_SQL, invoice.getCustomerId(), invoice.getOrderDate(), invoice.getPickupDate(), invoice.getReturnDate()
+                , invoice.getLateFee(), invoice.getInvoiceId());
     }
 
     @Override
     public void deleteInvoice(int invoiceId) {
-
+        jdbcTemplate.update(DELETE_INVOICE_SQL, invoiceId);
     }
 
     @Override
     public List<Invoice> getInvoiceByCustomerId(int customerId) {
-        return null;
+        return jdbcTemplate.query(SELECT_INVOICES_BY_CUSTOMER_SQL, this::mapRowToInvoice, customerId);
     }
-//    private JdbcTemplate jdbcTemplate;
-//
-//    //prepared statements
-//    private static final String INSERT_INVOICE_SQL =
-//            "insert into invoice (customer_id, order_date, pickup_date, return_date, late_fee) values (?, ?, ?, ?, ?)";
-//
-//    private static final String DELETE_INVOICE_SQL =
-//            "delete from invoice where invoice_id = ?";
-//
-//    private static final String SELECT_INVOICES_BY_CUSTOMER_SQL =
-//            "select * from invoice where customer_id = ?";
-//
-//    @Autowired
-//    public InvoiceDaoJdbcImpl(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
-//
-//    private Invoice mapRowToInvoice(ResultSet rs, int rowNum) throws SQLException {
-//        Invoice invoice = new Invoice();
-//        invoice.setInvoiceId(rs.getInt("invoice_id"));
-//        invoice.setCustomerId(rs.getInt("customer_id"));
-//        invoice.setOrderDate(rs.getObject("order_date", LocalDate.class));
-//        invoice.setPickupDate(rs.getObject("pickup_date", LocalDate.class));
-//        invoice.setReturnDate(rs.getObject("return_date", LocalDate.class));
-//        invoice.setLateFee(rs.getBigDecimal("late_fee"));
-//
-//        return invoice;
-//    }
-//
-//    @Override
-//    public Invoice addInvoice(Invoice invoice) {
-//        jdbcTemplate.update(INSERT_INVOICE_SQL, invoice.getCustomerId(), invoice.getOrderDate(), invoice.getPickupDate(), invoice.getReturnDate()
-//                , invoice.getLateFee());
-//        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
-//        invoice.setInvoiceId(id);
-//        return invoice;
-//    }
-//
-//    @Override
-//    public void deleteInvoice(int invoiceId) {
-//        jdbcTemplate.update(DELETE_INVOICE_SQL, invoiceId);
-//    }
-//
-//    @Override
-//    public List<Invoice> getInvoiceByCustomerId(int customerId) {
-//        return jdbcTemplate.query(SELECT_INVOICES_BY_CUSTOMER_SQL, this::mapRowToInvoice, customerId);
-//    }
 }
--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceItemDaoJdbcImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/InvoiceItemDaoJdbcImpl.java
@@ -2,6 +2,7 @@ package com.company.U1M6Summative.dao;
 
 import com.company.U1M6Summative.model.InvoiceItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,67 +12,72 @@ import java.util.List;
 
 @Repository
 public class InvoiceItemDaoJdbcImpl implements InvoiceItemDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    //prepared statements
+    private static final String INSERT_INVOICE_ITEM_SQL =
+            "insert into invoice_item (invoice_id, item_id, quantity, unit_rate, discount) values (?, ?, ?, ?, ?)";
+
+    private static final String DELETE_INVOICE_ITEM_SQL =
+            "delete from invoice_item where invoice_item_id = ?";
+
+    private static final String SELECT_INVOICE_ITEM_SQL =
+            "select * from invoice_item where invoice_item_id =?";
+
+    private static final String SELECT_ALL_INVOICE_ITEMS_SQL =
+            "select * from invoice_item";
+
+    private static final String UPDATE_INVOICE_ITEM_SQL =
+            "update invoice_item set invoice_id = ?, item_id = ?, quantity = ?, unit_rate = ?, discount = ? where invoice_item_id = ?";
+
+
+    @Autowired
+    public InvoiceItemDaoJdbcImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private InvoiceItem mapRowToInvoiceItem(ResultSet rs, int rowNum) throws SQLException {
+        InvoiceItem invoiceItem = new InvoiceItem();
+        invoiceItem.setInvoiceItemId(rs.getInt("invoice_item_id"));
+        invoiceItem.setInvoiceId(rs.getInt("invoice_id"));
+        invoiceItem.setItemId(rs.getInt("item_id"));
+        invoiceItem.setQuantity(rs.getInt("quantity"));
+        invoiceItem.setUnitRate(rs.getBigDecimal("unit_rate"));
+        invoiceItem.setDiscount(rs.getBigDecimal("discount"));
+
+        return invoiceItem;
+    }
+
     @Override
     public InvoiceItem addInvoiceItem(InvoiceItem invoiceItem) {
-        return null;
+        jdbcTemplate.update(INSERT_INVOICE_ITEM_SQL, invoiceItem.getInvoiceId(), invoiceItem.getItemId(), invoiceItem.getQuantity(), invoiceItem.getUnitRate(), invoiceItem.getDiscount());
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+        invoiceItem.setInvoiceItemId(id);
+        return invoiceItem;
     }
 
     @Override
     public InvoiceItem getInvoiceItem(int invoiceItemId) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_INVOICE_ITEM_SQL, this::mapRowToInvoiceItem, invoiceItemId);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public List<InvoiceItem> getAllInvoiceItems() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_INVOICE_ITEMS_SQL, this::mapRowToInvoiceItem);
     }
 
     @Override
     public void deleteInvoiceItem(int invoiceItemId) {
-
+        jdbcTemplate.update(DELETE_INVOICE_ITEM_SQL, invoiceItemId);
     }
 
     @Override
     public void updateInvoiceItem(InvoiceItem invoiceItem) {
-
+        jdbcTemplate.update(UPDATE_INVOICE_ITEM_SQL, invoiceItem.getInvoiceId(), invoiceItem.getItemId(), invoiceItem.getQuantity(), invoiceItem.getUnitRate(), invoiceItem.getDiscount(), invoiceItem.getInvoiceItemId());
     }
-//    private JdbcTemplate jdbcTemplate;
-//
-//    //prepared statements
-//    private static final String INSERT_INVOICE_ITEM_SQL =
-//            "insert into invoice_item (invoice_id, item_id, quantity, unit_rate, discount) values (?, ?, ?, ?, ?)";
-//
-//    private static final String DELETE_INVOICE_ITEM_SQL =
-//            "delete from invoice_item where invoice_item_id = ?";
-//
-//
-//    @Autowired
-//    public InvoiceItemDaoJdbcImpl(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
-//
-//    private InvoiceItem mapRowToInvoiceItem(ResultSet rs, int rowNum) throws SQLException {
-//        InvoiceItem invoiceItem = new InvoiceItem();
-//        invoiceItem.setInvoiceItemId(rs.getInt("invoice_item_id"));
-//        invoiceItem.setInvoiceId(rs.getInt("invoice_id"));
-//        invoiceItem.setItemId(rs.getInt("item_id"));
-//        invoiceItem.setQuantity(rs.getInt("quantity"));
-//        invoiceItem.setUnitRate(rs.getBigDecimal("unit_rate"));
-//        invoiceItem.setDiscount(rs.getBigDecimal("discount"));
-//
-//        return invoiceItem;
-//    }
-//
-//    @Override
-//    public InvoiceItem addInvoiceItem(InvoiceItem invoiceItem) {
-//        jdbcTemplate.update(INSERT_INVOICE_ITEM_SQL, invoiceItem.getInvoiceId(), invoiceItem.getItemId(), invoiceItem.getQuantity(), invoiceItem.getUnitRate(), invoiceItem.getDiscount());
-//        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
-//        invoiceItem.setInvoiceItemId(id);
-//        return invoiceItem;
-//    }
-//
-//    @Override
-//    public void deleteInvoiceItem(int invoiceItemId) {
-//        jdbcTemplate.update(DELETE_INVOICE_ITEM_SQL, invoiceItemId);
-//    }
 }
