7cc16a25039c0f52f56131562b96d2e563088cd3
sandynism <31261960+Sandynism@users.noreply.github.com>
2019-08-28 13:33:21 -0400

    edited yaml and added initial test files

---
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/dao/CustomerDaoTest.java
@@ -0,0 +1,33 @@
+package com.company.U1M6Summative.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addCustomer() {
+    }
+
+    @Test
+    public void getCustomer() {
+    }
+
+    @Test
+    public void getAllCustomers() {
+    }
+
+    @Test
+    public void updateCustomer() {
+    }
+
+    @Test
+    public void deleteCustomer() {
+    }
+}
