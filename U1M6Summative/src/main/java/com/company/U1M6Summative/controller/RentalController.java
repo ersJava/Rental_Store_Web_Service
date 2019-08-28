dbdf8cc62b1690350cf661bf0430e0c039fec639
Beth <ersergeant@gmail.com>
2019-08-28 15:23:12 -0400

    created customer dao test and item dao test

--- U1M6Summative/src/main/java/com/company/U1M6Summative/controller/RentalController.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/controller/RentalController.java
@@ -4,11 +4,8 @@ import com.company.U1M6Summative.dao.CustomerDao;
 import com.company.U1M6Summative.dao.InvoiceDao;
 import com.company.U1M6Summative.dao.InvoiceItemDao;
 import com.company.U1M6Summative.dao.ItemDao;
-import com.company.U1M6Summative.model.Customer;
-import com.company.U1M6Summative.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import sun.misc.Request;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RentalController {
@@ -20,33 +17,33 @@ public class RentalController {
     InvoiceItemDao invoiceItemDao;
     @Autowired
     ItemDao itemDao;
-
-    @RequestMapping(value = "/customers", method = RequestMethod.POST)
-    public Customer createCustomer(@RequestBody Customer customer) {
-        customerDao.addCustomer(customer);
-        return customer;
-    }
-
-    @RequestMapping(value = "/items", method = RequestMethod.POST)
-    public Item createItem(@RequestBody Item item) {
-        itemDao.addItem(item);
-        return item;
-    }
-
-    @RequestMapping(value = "/items/item_id", method = RequestMethod.GET)
-    public Item getItemById(@PathVariable(name="item_id") int item_id) {
-        return itemDao.getItem(item_id);
-    }
-
-    @RequestMapping(value = "/items/item_id", method = RequestMethod.DELETE)
-    public void deleteItemById(@PathVariable(name="item_id") int item_id) {
-        itemDao.deleteItem(item_id);
-    }
-
-    @RequestMapping(value = "/items/item_id", method= RequestMethod.PUT)
-    public void updateItemById(@RequestBody Item item, @PathVariable(name="item_id") int item_id) {
-        itemDao.updateItem(item);
-    }
+//
+//    @RequestMapping(value = "/customers", method = RequestMethod.POST)
+//    public Customer createCustomer(@RequestBody Customer customer) {
+//        customerDao.addCustomer(customer);
+//        return customer;
+//    }
+//
+//    @RequestMapping(value = "/items", method = RequestMethod.POST)
+//    public Item createItem(@RequestBody Item item) {
+//        itemDao.addItem(item);
+//        return item;
+//    }
+//
+//    @RequestMapping(value = "/items/item_id", method = RequestMethod.GET)
+//    public Item getItemById(@PathVariable(name="item_id") int item_id) {
+//        return itemDao.getItem(item_id);
+//    }
+//
+//    @RequestMapping(value = "/items/item_id", method = RequestMethod.DELETE)
+//    public void deleteItemById(@PathVariable(name="item_id") int item_id) {
+//        itemDao.deleteItem(item_id);
+//    }
+//
+//    @RequestMapping(value = "/items/item_id", method= RequestMethod.PUT)
+//    public void updateItemById(@RequestBody Item item, @PathVariable(name="item_id") int item_id) {
+//        itemDao.updateItem(item);
+//    }
 
 
 
